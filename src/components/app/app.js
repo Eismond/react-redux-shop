f35578fcf813f05414fef0a660a5b3e5cbf03869
Eismond <a.eismond@gmail.com>
2021-03-30 23:29:48 +0300

    Created base Route

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import withBookstoreService from '../hoc';
+import {
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import { HomePage, CartPage } from '../pages';
 
 import './app.css'
 
-const App = ({ bookstoreService }) => {
-    console.log('test connect', bookstoreService.getBooks());
-    return (<div>App</div>)
+const App = () => {
+    return (<div>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/cart-page">CP</Link>
+            </li>
+        </ul>
+        <Switch>
+            <Route
+                path="/"
+                component={HomePage}
+                exact />
+            <Route
+                path="/cart-page"
+                component={CartPage} />
+        </Switch>
+    </div>)
 }
 
-export default withBookstoreService()(App);
+export default App;
